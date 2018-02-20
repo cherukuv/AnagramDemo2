f876e344fa6b27a770a609f9bec858fc545058ec
cheruku <cheruku.vignan@gmail.com>
2018-02-20 03:15:10 -0500

    Find Anagrams from given string/text

---
+++ src/AnagramDemo2.java
@@ -0,0 +1,28 @@
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+import java.util.stream.*;
+
+public class AnagramDemo2{
+	  
+    public static void main(String[] args) throws IOException {
+    	 System.out.println("Please enter text or paragraph\n");
+		 Scanner sc = new Scanner(System.in);
+		 getAnagramsFromInput(new StringReader(sc.nextLine()))
+         .forEach(System.out::println);
+
+    }
+    
+    private static String standardize(String check) {
+        return Stream.of(check.split("")).sorted().collect(Collectors.joining());
+    }
+
+    public static List<Set<String>> getAnagramsFromInput(Reader reader) {
+    Map<String, Set<String>> map = new BufferedReader(reader).lines()
+                                     .flatMap(Pattern.compile("\\W+")::splitAsStream)
+                                     .collect(Collectors.groupingBy(AnagramDemo2::standardize, Collectors.toSet()));
+        return map.values().stream().filter(list -> list.size() > 1).collect(Collectors.toList());
+    }
+
+}
